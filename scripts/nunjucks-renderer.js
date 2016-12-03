6b52f139963ae2379bca99a4e5eb03a445957b07
Luke Tillman <luke.tillman@gmail.com>
2016-12-03 11:11:34 -0700

    Get nunjucks rendering working
    - Add custom theme folder with basic NJ file
    - Add script with NJ renderer

---
+++ scripts/nunjucks-renderer.js
@@ -0,0 +1,18 @@
+const nj = require('nunjucks');
+const path = require('path');
+
+// Get path to layouts and create NJ environment
+const layoutsPath = path.join(hexo.theme.base, 'layouts');
+const njEnv = nj.configure(layoutsPath);
+
+function njRender(data, locals) {
+  return njEnv.renderString(data.text, locals);
+}
+
+njRender.compile = function(data, locals) {
+  let template = new nj.Template(data.text, njEnv);
+  return template.render.bind(template);
+};
+
+// Register with hexo
+hexo.extend.renderer.register('njk', 'html', njRender, true);
