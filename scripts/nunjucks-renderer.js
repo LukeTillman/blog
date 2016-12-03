e1f9ed31de0406a2ac6cbf7d655bd0fad84141a4
Luke Tillman <luke.tillman@gmail.com>
2016-12-03 15:21:22 -0700

    Basic page and post layouts, list articles on index

--- scripts/nunjucks-renderer.js
+++ scripts/nunjucks-renderer.js
@@ -8,6 +8,11 @@ let noCache = hexo.env.args._[0] === 'serve' ? true : false;
 const layoutsPath = path.join(hexo.theme.base, 'layout');
 const njEnv = nj.configure(layoutsPath, { noCache });
 
+// Add a custom filter for debugging
+njEnv.addFilter('debug', function(obj) {
+  debugger;
+});
+
 function njRender(data, locals) {
   return njEnv.renderString(data.text, locals);
 }
