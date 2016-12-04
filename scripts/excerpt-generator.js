765cb9f985810c3196e8a371c194fef5bd301689
Luke Tillman <luke.tillman@gmail.com>
2016-12-03 17:19:07 -0700

    Add excerpt generator for posts

---
+++ scripts/excerpt-generator.js
@@ -0,0 +1,11 @@
+const excerptHtml = require('excerpt-html');
+
+// Add a custom filter to generate the excerpt for a post
+hexo.extend.filter.register('after_post_render', function(data) {
+  if (data.excerpt !== '') return data;
+
+  // Use first paragraph or everything up to <-- more -->
+  let excerpt = excerptHtml(data.content, { stripTags: true, pruneLength: 250, pruneString: '...', pruneSeperator: ' ' });
+  data.excerpt = excerpt;
+  return data;
+});
