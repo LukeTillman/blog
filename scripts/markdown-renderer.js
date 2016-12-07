bcbd8ffb72c86642b05b0cb81bd134e29392938d
Luke Tillman <luke.tillman@gmail.com>
2016-12-07 10:49:17 -0700

    Switch to customized markdown renderer
    - This gives me total control over things like syntax highlighting with highlight.js
    - Luckily, most of this code was stolen from the KillrVideo GitHub site generator

---
+++ scripts/markdown-renderer.js
@@ -0,0 +1,34 @@
+const marked = require('marked');
+const highlightJs = require('highlight.js');
+
+// Override some of the default rendering
+const renderer = new marked.Renderer();
+
+// Add highlight.js syntax highlighting if a language is specified
+renderer.code = function(code, language) {
+  if (language) {
+    return `<pre><code class="hljs">${highlightJs.highlightAuto(code, [ language ]).value}</pre></code>`;
+  }
+  return `<pre><code>${code}</code></pre>`;
+};
+
+// Open external links in a new window
+renderer.link = function(href, title, text) {
+  let titleAttr = title ? ` title="${title}"` : '';
+  let targetAttr = href.startsWith('http') ? ` target="_blank"` : '';
+  return `<a href="${href}"${titleAttr}${targetAttr}>${text}</a>`;
+};
+
+// Set default options for markdown rendering
+marked.setOptions({
+  gfm: true,
+  renderer
+});
+
+// The renderer for hexo
+function renderMarkdown(data, locals) {
+  return marked(data.text);
+}
+
+// Add renderer to hexo
+hexo.extend.renderer.register('md', 'html', renderMarkdown, true);
