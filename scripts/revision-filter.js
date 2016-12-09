ab85db14091fd3ff8fb0489c04262e84a2479549
Luke Tillman <luke.tillman@gmail.com>
2016-12-09 15:44:12 -0700

    Add git revision info and link in footer

---
+++ scripts/revision-filter.js
@@ -0,0 +1,13 @@
+const gitRev = require('git-rev');
+
+let revisionPromise = new Promise(resolve => gitRev.short(resolve));
+
+// Use a template_locals filter so the revision info is available on all templates
+hexo.extend.filter.register('template_locals', function(locals) {
+  // Add git revision info to data
+  return revisionPromise.then(shortRev => {
+    locals.page.revision = shortRev;
+    locals.page.revision_url = `${hexo.config.git.repo_url}/${shortRev}`
+    return locals;
+  });
+});
